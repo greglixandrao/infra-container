71d01c7293f1859eb2e1fb384941934f461f1366
Greg Lixandrão <contato@greglixandrao.com>
2024-07-06 18:44:31 -0300

    format files

--- env/homolog/main.tf
+++ env/homolog/main.tf
@@ -1,7 +1,7 @@
 module "Homolog" {
   source = "../../infra"
   name = "homolog"
-  description = "Django-Aplication-Homolog"
+  description = "jango-aplication-homolog"
   environment = "homolog"
   server = "t2.micro"
   max = 2
--- infra/providers.tf
+++ infra/providers.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
-	aws = {
-		source = "hashicorp/aws"
-		version = "~> 4.16"
-	}
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
   }
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-	profile = "default"
-	region = "us-west-2"
+  profile = "default"
+  region  = "us-west-2"
 }
 
